cc433e3a0aca80609e5c052bf2a2e1b5211fc62d
Washington Lima <136650423+Wasviana@users.noreply.github.com>
2023-07-21 19:54:37 -0300

    Projeto bancario da aula senac aula 10
    Esse projeto é um desafio da aula com o professor William

---
+++ banco_02.py
@@ -0,0 +1,138 @@
+def menu():
+    menu = """
+    ============  M E N U  ===========
+    [d]  Depositar
+    [s]  Sacar
+    [e]  Extrato
+    [nc] Nova Conta
+    [lc] Listar Contas
+    [nu] Novo Usuário
+    [q]  Sair
+    =================================
+    ==>> """
+    return input(menu)
+
+def depositar(saldo, valor, extrato, /):
+    if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+            print("=== Depósito realizado com sucesso! ===")
+        
+    else:
+            print("@@@ Operação Falhou! O valor informado é inválido! @@@")
+        
+    return saldo, extrato 
+
+def saque(*,saldo, valor, extrato, limite, numero_saques, LIMITE_SAQUES):
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+    if excedeu_saldo:
+            print("@@@ Operação Falhou. Você não tem saldo suficiente! @@@")
+
+    elif excedeu_limite:
+            print("@@@ Operação Falhou. O valor do saque excede o limite. @@@")
+
+    elif excedeu_saques:
+            print("@@@ Operação Falhou. Número máximo de saques excedidos. @@@")
+
+    elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            print(" === Saque realizado com sucesso! ===")
+            numero_saques += 1
+
+    else:
+            print("@@@ Operação Falhou! O valor informado é inválido! @@@")
+        
+    return saldo, extrato
+
+def exibir_extrato(saldo, /, *, extrato):
+    print("\n=========== EXTRATO ===========")
+    print(f"\nSaldo: R$ {saldo:.2f}")
+            # print("Não foram realizadas movimentações." if not extrato else extrato)
+    print("@@@  Não foram realizadas movimentações. @@@" if extrato == "" else extrato)
+    print("\n===============================")
+
+
+
+
+def criar_usuario():
+    cpf = input("Informe o CPF (Somente números): ")
+    usuario = 0 
+
+    if usuario:
+        print("@@@ CPF já Cadastrado @@@")
+    
+    nome = input("Informe o Nome Completo: ")
+    data_de_nascimento = input("Informe a Data de Nascimento (dd-mm--aaa): ")
+    endereco = input("Informe o Endereço (Logradouro, Numero, Bairro, Cidade/Sigla estado): ")
+
+    print("=== Usuário Criado com sucesso ! ===")
+
+def criar_conta():
+    cpf = input("Informe o CPF (Somente números): ")
+    usuario = 0
+
+    if usuario:
+        print("=== Conta Criada com sucesso ===")
+        return {"Nome":nome, "Data-de-Nascimento":data_de_nascimento, "Endereço":endereco}
+   
+    else:
+        print(" @@@ Usuário não encontrado. Favor cadastar usuário ! @@@")
+
+def filtrar_usuario():
+    usuarios_filtrados = [user for user in usuarios if usuarios["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+def listar_contas():
+    pass
+
+def main():
+    LIMITE_SAQUES = 3
+    AGENCIA = "0001"
+
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    usuarios = []
+    contas = []
+
+    while True:
+        opcao = menu()
+
+        if opcao == "d":
+            valor = float(input("Informe o valor do depósito: "))
+
+            depositar(saldo, valor,extrato)
+
+        elif opcao == "s":
+            valor = float(input("Informe o valor do saque: "))
+
+            saque(saldo=saldo, valor=valor, extrato=extrato, limite=limite, numero_saques=numero_saques,
+             LIMITE_SAQUES=LIMITE_SAQUES)
+
+        elif opcao == "e":
+            
+            exibir_extrato(saldo, extrato=extrato)
+
+        elif opcao == "nc":
+            criar_conta()
+
+        elif opcao == "lc":
+            listar_contas()
+
+        elif opcao == "nu":
+            criar_usuario()
+
+        elif opcao == "q":
+            break
+
+        else:
+            print("Opção Inválida, Selecione nova operação:")
+
+
+
+
